fa76e08f7180d1ae79dbe8d02af165cd277805e0
Michael Marks <mmarks315@charter.net>
2018-03-01 18:00:10 -0600

    added keep alive controller

---
+++ HUTWebAPI/Controllers/KeepAliveController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Web.Http;
+
+namespace HUTWebAPI.Controllers
+{
+    public class KeepAliveController : ApiController
+    {
+        /// <summary>
+        /// Keeping the api alive
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IHttpActionResult Get()
+        {            
+            return Content(HttpStatusCode.OK, "Staying Alive!");            
+        }
+    }
+}
