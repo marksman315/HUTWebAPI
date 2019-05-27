4a09448d8f00c7633eaa393e2f893806638ad90d
Michael Marks <mmarks315@charter.net>
2019-05-27 15:50:32 -0500

    Added CORS for localhost

--- HUTWebAPI/Controllers/FreezerInventoryController.cs
+++ HUTWebAPI/Controllers/FreezerInventoryController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using HUTBusinessLayer.API;
 
 namespace HUTWebAPI.Controllers
-{
+{    
+    [EnableCors(origins: "http://localhost:44319", headers: "*", methods: "*")]
     public class FreezerInventoryController : ApiController
     {
         [HttpGet]
